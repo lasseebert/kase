c6f7c8132354c14879f1d88a802f687714414831
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2016-03-30 22:19:56 +0200

    Implemented #kase and #ok! module methods

--- lib/kase.rb
+++ lib/kase.rb
@@ -1,4 +1,29 @@
+require "kase/switcher"
 require "kase/version"
 
 module Kase
+  module_function
+
+  def kase(*values, &block)
+    Switcher.new(*values).switch(&block)
+  end
+
+  def ok!(*values, &block)
+    Switcher.new(*values).switch do
+      on(:ok) do |*result|
+        if block_given?
+          yield(*result) if block_given?
+        else
+          case result.size
+          when 0
+            nil
+          when 1
+            result.first
+          else
+            result
+          end
+        end
+      end
+    end
+  end
 end
--- spec/kase_spec.rb
+++ spec/kase_spec.rb
@@ -1,7 +1,47 @@
 require "kase"
 
 RSpec.describe Kase do
+  include Kase
+
   it "has a version number" do
     expect(Kase::VERSION).not_to be nil
+  end
+
+  describe "#kase" do
+    it "delegates to switcher" do
+      value = kase :ok, "RESULT" do
+        on(:ok) { |result| result }
+      end
+
+      expect(value).to eq("RESULT")
+    end
+  end
+
+  describe "#ok!" do
+    it "returns nil on one value" do
+      result = ok! :ok
+      expect(result).to be_nil
+    end
+
+    it "returns a single result on two values" do
+      result = ok! :ok, "RESULT"
+      expect(result).to eq("RESULT")
+    end
+
+    it "returns an array on more values" do
+      result = ok! :ok, "RESULT", "ONE MORE"
+      expect(result).to eq(["RESULT", "ONE MORE"])
+    end
+
+    it "yields" do
+      result = ok! :ok, "RESULT" do |res|
+        res + " yielded"
+      end
+      expect(result).to eq("RESULT yielded")
+    end
+
+    it "raises on no match" do
+      expect { ok! :error }.to raise_error(Kase::NoMatchError)
+    end
   end
 end
